8674fdaee83ebe65756a81b578fcc0894126758d
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 10:15:57 -0600

    Add files via upload

---
+++ estaciones_spyuc_xcun1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_spyuc_xcun1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Av. 135 Supermanzana 99, 77536 Cancún, Q.R.", "description": "XCUN1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -86.8773325, 21.1432876, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Kanasín, Yucatán", "description": "SPYUC1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -89.5639005, 20.944148, 0.0 ] } }
+]
+}
